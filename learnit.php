45a4166641fe5fb9614b18bc2c180c120d319fd7
KAWAMOTO-TAKESHITA <tk0987281@dt050581.otc.edu>
2024-08-26 10:10:19 -0500

    initial commit

---
+++ learnit.php
@@ -0,0 +1,34 @@
+<?php
+
+// echo("Hello World\n");
+// $opening = "Aloha\n";
+// echo($opening ."Tate");
+
+
+$count = 6;
+$price = 9.58;
+$first_name = "bob";
+$first_name = 'Bob';
+$is_valid = true;
+
+$nullVariable;
+echo @$nullVariable;
+
+
+define("PI", 3.14);
+
+
+$games = ['Animal Crosing ', "Harry Potter"];
+// echo($games);
+
+// print_r($games)
+
+
+$student1= [
+    'name' => "mitzy",
+    'id' => 123456,
+    'grade' => 'A'
+];
+
+echo($student1['name']);
+print_r($student1);
